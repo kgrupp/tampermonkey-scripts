d352e4f22e0aa35c28314a5907fc16760e781a2b
Konstantin Grupp <coding@konstantin-grupp.de>
2023-07-23 08:32:03 +0200

    Fix youtube-hide-shorts.user.js

--- youtube-hide-shorts.user.js
+++ youtube-hide-shorts.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Hide YouTube Shorts
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  More efficient without YouTube shorts.
 // @author       Konstantin Grupp
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        GM_addStyle
-// @downloadURL  https://github.com/kgrupp/tampermonkey-scripts/raw/master/youtube-hide-shorts.user.js
+// @downloadURL  https://github.com/kgrupp/tampermonkey-scripts/raw/main/youtube-hide-shorts.user.js
 // ==/UserScript==
 
 (function() {
